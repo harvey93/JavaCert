db29791b3d70d04b5f33e850007b9177ba12ff20
Hrant <harvey.mirijanyan@gmail.com>
2017-06-24 18:32:36 -0700

    Start fatorial

--- CertProbs/src/aAProbs/StringReverse.java
+++ CertProbs/src/aAProbs/StringReverse.java
@@ -3,6 +3,10 @@ package aAProbs;
 public class StringReverse {
 	
 	public static String reverse(String str) {
-		return str;
+		StringBuilder sb = new StringBuilder();
+		for(int i = str.length() - 1; i >= 0; i--){
+			sb.append(str.charAt(i));
+		}
+		return sb.toString();
 	} 
 }
