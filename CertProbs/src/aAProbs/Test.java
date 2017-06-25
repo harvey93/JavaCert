efe032f373e553880ff2bc2dab4523d6a66e0afd
Hrant <harvey.mirijanyan@gmail.com>
2017-06-25 12:19:01 -0700

    Add defaults to in Place String Reverse

--- CertProbs/src/aAProbs/Test.java
+++ CertProbs/src/aAProbs/Test.java
@@ -3,7 +3,8 @@ package aAProbs;
 public class Test {
 	
 	public static void main(String args[]) {
-		System.out.println(Palindrome.isPalindrome("abbac"));
+//		System.out.println(Palindrome.isPalindrome("abbac"));
+		System.out.println(StringReverse.reverseIP("hello"));
 	}
 
 }
