1d9fdc019a481cc49cbf2d46e13ea26756955cf5
Hrant <harvey.mirijanyan@gmail.com>
2017-06-26 13:14:13 -0700

    Start most common letter

---
+++ CertProbs/src/aAProbs/MostCommonLetter.java
@@ -0,0 +1,11 @@
+package aAProbs;
+import java.util.*;
+
+public class MostCommonLetter {
+	
+	public static HashMap<Character, Integer> mostCommon(String str){
+		HashMap<Character, Integer> result = new HashMap<Character, Integer>();
+		
+		return result;
+	}
+}
