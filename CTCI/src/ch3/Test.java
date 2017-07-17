f7da75a232c8d3671d1b7c90bb25401634b65681
Hrant <harvey.mirijanyan@gmail.com>
2017-07-17 14:26:05 -0700

    Remove tests from main for NodeStack

--- CTCI/src/ch3/Test.java
+++ CTCI/src/ch3/Test.java
@@ -5,21 +5,6 @@ import java.util.Stack;
 public class Test {
 	
 	public static void main(String[] args) {
-		NodeStack<Double> stack = new NodeStack<>();
-		stack.push(10.0);
-		stack.push(12.4);
-		stack.push(13.3);
-		stack.push(14.2);
-		stack.push(15.1);
 
-		System.out.println(stack.pop());
-		System.out.println(stack.pop());
-		System.out.println(stack.pop());
-		System.out.println(stack.pop());
-		System.out.println(stack.pop());
-		
-		stack.push(18.2);
-		System.out.println("----------------");
-		System.out.println(stack);
 	}
 }
