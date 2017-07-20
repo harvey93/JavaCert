b7467af43fd0eaebd84bbb848e5c0eba32de5d33
Hrant <harvey.mirijanyan@gmail.com>
2017-07-19 22:14:25 -0700

    Remove Tests from StackQueue

--- CTCI/src/ch3/Test.java
+++ CTCI/src/ch3/Test.java
@@ -7,19 +7,7 @@ public class Test {
 	
 	
 	public static void main(String[] args) throws StackEmptyException{
-		StackQueue<Integer> queue = new StackQueue<>();
-		queue.queue(10);
-		queue.queue(20);
-		queue.queue(30);
-		queue.queue(40);
-		queue.queue(50);
-		queue.dequeue();
-		queue.dequeue();
-		queue.dequeue();
-		queue.dequeue();
-		queue.dequeue();
-		queue.dequeue();
-		System.out.println(queue.toString());
+
 	}
 	
 	
