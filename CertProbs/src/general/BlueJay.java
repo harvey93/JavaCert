922bad098acfed010fe0a4b7da3fbe0f283fedd9
Hrant <harvey.mirijanyan@gmail.com>
2017-07-19 19:48:54 -0700

    Add tests for multi catch statements

---
+++ CertProbs/src/general/BlueJay.java
@@ -0,0 +1,14 @@
+package general;
+
+public class BlueJay extends Bird{
+
+	public BlueJay(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void write(){
+		System.out.println("BlueJay write");
+	}
+
+}
