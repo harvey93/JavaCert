65ee11e4e8991f8f32f5443392a8798c304dbf72
Hrant <harvey.mirijanyan@gmail.com>
2017-07-21 12:16:50 -0700

    Remove tests

--- CTCI/src/ch3/Test.java
+++ CTCI/src/ch3/Test.java
@@ -5,17 +5,7 @@ package ch3;
 public class Test {
 	
 	public static void main(String[] args) {
-		AnimalShelter shelter = new AnimalShelter();
-		shelter.enqueue(new Cat("Cat1"));
-		shelter.enqueue(new Dog("Hrant"));
-		shelter.enqueue(new Dog("Fredo"));
-		shelter.enqueue(new Dog("Paul"));
-//		shelter.dequeuDog();
-		shelter.enqueue(new Cat("Mittens"));
-		shelter.enqueue(new Cat("Kat"));
-//		shelter.dequeuCat();
-		shelter.dequeueAny();
-		System.out.println(shelter);
+
 	}
 
 }
