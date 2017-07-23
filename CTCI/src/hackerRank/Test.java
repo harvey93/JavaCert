0b5f2b1dfa2835ee201e5bb504c6f8366f3f432f
Hrant <harvey.mirijanyan@gmail.com>
2017-07-22 21:55:06 -0700

    Remove tests

--- CTCI/src/hackerRank/Test.java
+++ CTCI/src/hackerRank/Test.java
@@ -3,7 +3,5 @@ package hackerRank;
 public class Test {
 	
 	public static void main(String[] args) {
-		StringProbs stringProbs = new StringProbs();
-		System.out.println(stringProbs.numbersNeeded("edc", "cba"));
 	}
 }
