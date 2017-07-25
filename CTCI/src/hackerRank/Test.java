64d878377e6855eadf06bcd3c08baba0fe2f6801
Hrant <harvey.mirijanyan@gmail.com>
2017-07-25 16:12:00 -0700

    Start runningMedian

--- CTCI/src/hackerRank/Test.java
+++ CTCI/src/hackerRank/Test.java
@@ -1,27 +1,18 @@
 package hackerRank;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Test {
 	
 	public static void main(String[] args) {
-		BMaxHeap maxHeap = new BMaxHeap();
+//		PriorityQueue<Integer> myQueue = new PriorityQueue<>();
+//		myQueue.add(15);
+//		myQueue.add(12);
+//		myQueue.add(34);
+//		myQueue.add(0);
+//		System.out.println(myQueue);
 		
-		maxHeap.add(10);
-		maxHeap.add(15);
-		maxHeap.add(20);
-		maxHeap.add(6);
-		maxHeap.add(40);
-		maxHeap.heapSort();
-		System.out.println(maxHeap);
-		BMinHeap minHeap = new BMinHeap();
-		minHeap.add(10);
-		minHeap.add(15);
-		minHeap.add(20);
-		minHeap.add(6);
-		minHeap.add(40);
-		minHeap.heapSort();
-		System.out.println(minHeap);
 	}
 }
 
