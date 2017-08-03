caecb1f47ad3055742535dbf087f730118f366a4
Hrant <harvey.mirijanyan@gmail.com>
2017-08-02 17:34:51 -0700

    Start HashMap

--- CTCI/src/cache/Test.java
+++ CTCI/src/cache/Test.java
@@ -6,13 +6,7 @@ import java.util.Arrays;
 public class Test {
 	
 	public static void main(String[] args) throws Exception{
-		LinkedList<String, Integer> list = new LinkedList<>();
-		list.add("first", 1);
-		list.add("second", 2);
-		list.add("third", 3);
 		
-		
-		System.out.println(list);
 	}
 
 }
