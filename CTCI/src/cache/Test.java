a0556abc54b9c3d4f81bc28d920961c307b1a25e
Hrant <harvey.mirijanyan@gmail.com>
2017-08-03 17:50:32 -0700

    Remove tests

--- CTCI/src/cache/Test.java
+++ CTCI/src/cache/Test.java
@@ -6,14 +6,6 @@ import java.util.Arrays;
 public class Test {
 	
 	public static void main(String[] args) throws Exception{
-		LRUCache<String, Integer> cache = new LRUCache<>(4);
-		cache.set("Hrant", 23);
-		cache.set("Varuzhan", 24);
-		cache.set("Khacho", 20);
-		cache.set("Vazgen", 35);
-		cache.set("Anush", 22);
-		cache.set("Vazgen", 0);
-		System.out.println(cache);
 	}
 
 }
