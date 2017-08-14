f3b1235737de2e4e44bf5957d8a9daba5859a89c
Hrant <harvey.mirijanyan@gmail.com>
2017-08-14 12:59:20 -0700

    Add Bird and Dog

---
+++ Patterns/src/strategy/Bird.java
@@ -0,0 +1,9 @@
+package strategy;
+
+public class Bird extends Animal{
+	
+	public Bird(){
+		flyingType = new ItFlys();
+	}
+
+}
---
+++ Patterns/src/strategy/Dog.java
@@ -0,0 +1,9 @@
+package strategy;
+
+public class Dog extends Animal{
+	
+	public Dog(){
+		flyingType = new CantFly(); 
+	}
+
+}
