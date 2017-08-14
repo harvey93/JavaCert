251633662760e0255258a778b471dd0d4146e642
Hrant <harvey.mirijanyan@gmail.com>
2017-08-14 16:50:13 -0700

    Start UFOShipBuilding

---
+++ Patterns/src/AbstractFactory/UFOShipBuilding.java
@@ -0,0 +1,11 @@
+package AbstractFactory;
+
+public class UFOShipBuilding extends EnemyShipBuilding{
+
+	@Override
+	protected EnemyShip makeEnemyShip(String typeOfShip) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
