7b6708714b6bf493079065ae7748c4eee2c9ba15
Hrant <harvey.mirijanyan@gmail.com>
2017-08-22 16:27:11 -0700

    Statt nextNUm

---
+++ CTCI/src/ch5/NextNum.java
@@ -0,0 +1,16 @@
+package ch5;
+
+public class NextNum {
+	
+	public static int getNext(){
+		return 5;
+	}
+	
+	public static int getNextLargest(){
+		return 5;
+	}
+	public static int getNextSmallest(){
+		return 5;
+	}
+
+}
