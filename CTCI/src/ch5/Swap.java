99ffd7b8b7e761e1e044d744bd40f1484db74e23
Hrant <harvey.mirijanyan@gmail.com>
2017-08-22 21:26:23 -0700

    Start pairSwap

---
+++ CTCI/src/ch5/Swap.java
@@ -0,0 +1,15 @@
+package ch5;
+
+public class Swap {
+	
+	public static int swap(int i){
+		int x = 0xaaaaaa;
+		int odds = i & x;
+		int y = 0x5555555;
+		int evens = i & y;
+		System.out.println(Integer.toBinaryString(odds >> 1));
+		System.out.println(Integer.toBinaryString(evens << 1));
+		return i;
+	}
+
+}
