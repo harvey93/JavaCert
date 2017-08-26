10a09b3002777b57c3a99f9999b50f2d8e956295
Hrant <harvey.mirijanyan@gmail.com>
2017-08-25 19:46:21 -0700

    Start oveloaded pathsMemo

--- CTCI/src/ch8/MatrixProbs.java
+++ CTCI/src/ch8/MatrixProbs.java
@@ -2,7 +2,12 @@ package ch8;
 
 public class MatrixProbs {
 
-	public static int paths(int[][] matrix){
+	public static int pathsMemo(int[][] matrix){
+		int[][] memo = new int[matrix.length][matrix[0].length];
+		return pathsMemo(matrix, memo);
+	}
+	
+	public static int pathsMemo(int[][] matrix, int[][] memo){
 		return 5;
 	}
 }
