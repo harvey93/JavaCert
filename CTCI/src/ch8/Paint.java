8f0bf2f12900648c68264cecaf74cb09e361c570
Hrant <harvey.mirijanyan@gmail.com>
2017-08-28 14:27:24 -0700

    Add tests

--- CTCI/src/ch8/Paint.java
+++ CTCI/src/ch8/Paint.java
@@ -1,5 +1,17 @@
 package ch8;
 
 public class Paint {
+	
+	public static boolean fillPaint(Color[][] screen, int row, int col, Color ncolor){
+		if(screen[row][col] == ncolor){
+			return false;
+		}
+		return true;
+	}
 
+}
+
+
+enum Color{
+	B, W, R, Y, G;
 }
